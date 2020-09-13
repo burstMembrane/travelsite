486e760d0b8c0eaf27b61f3e9a9872b0b7f9a617
Liam Power <liamfpower@gmail.com>
2020-09-13 21:22:30 +1000

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+
+    // set entry point (index) for webpack
+    entry: './app/assets/scripts/App.js',
+    // set output to custom path
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    },
+    devServer: {
+        before: (app, server) => {
+            server._watch('./app/**/*.html')
+        },
+        host: '0.0.0.0',
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000
+    },
+    // set to development
+    mode: 'development',
+    // update on file change
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: ['style-loader', {
+                loader: 'css-loader',
+                // allow imports in css files for postcss
+                options: {
+                    importLoaders: 1
+                }
+            }, 'postcss-loader']
+        }]
+    },
+
+}
