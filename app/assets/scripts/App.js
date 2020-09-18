44472f184abecbc1932f0526aa43d422bc2186f6
Liam Power <liamfpower@gmail.com>
2020-09-18 15:11:24 +1000

    finished modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,29 +3,33 @@ import "../styles/styles.css";
 
 // import javascript modules
 import MobileMenu from './modules/MobileMenu';
-import OnScroll from './modules/OnScroll'
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 
 
-let menu = new MobileMenu;
-
-const header = document.querySelector('.site-header__logo')
-
-const hideNav = () => !header.classList.contains('hide') ? header.classList.add('hide') : null
-const showNav = () => header.classList.remove('hide')
-
-let stickyHeader = new StickyHeader()
-
+new MobileMenu();
+new StickyHeader()
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75)
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 95)
 
-// let windowScroll = new OnScroll({
-//     threshold: 300,
-//     ifTrue: hideNav,
-//     ifFalse: showNav
-// });
 
+let modal;
+
+
+// only import modal code when user clicks on a link -- saves initial page load.
+document.querySelectorAll(".open-modal").forEach((el) => {
+    el.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (typeof(modal) == "undefined") {
+            import ( /* webpackChunkName:"modal" */ './modules/Modal').then((x) => {
+                modal = new x.default()
+                setTimeout(() => modal.openTheModal(), 20)
+            }).catch((err) => console.log(err))
+        } else {
+            modal.openTheModal()
+        }
+    })
+})
 
 // ! allow hot reloading of the files in project
 if (module.hot) {
